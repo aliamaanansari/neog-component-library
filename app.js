267e917ee2c0b16a780322aa067cd0b966d193f4
Ali aman Ansari <aliamaan071@gmail.com>
2022-02-20 02:23:01 +0530

    feature: added avatar component

---
+++ app.js
@@ -0,0 +1,31 @@
+const sideBar = document.querySelector('.side-bar')
+
+const navOptions = {
+  Avatar: ['large', 'medium', 'small'],
+}
+
+const createNavbar = () => {
+  let output = ''
+  Object.keys(navOptions).forEach((key) => {
+    /*html*/
+    output += `
+        <div class="nav-item">
+          <div class="nav-items-heading">
+            <a>${key}</a>
+          </div>
+           <div class="nav-sub-items">
+         ${createSubMenus(navOptions[key], key)}</div>
+         </div>`
+  })
+  return output
+}
+const createSubMenus = (menus = [], key) => {
+  let output = ''
+  menus.forEach((item) => {
+    /*html*/
+    output += `<a href="#${`${item}-${key}`.replace(/\s/g, '-')}">${item}</a>`
+  })
+  return output
+}
+
+sideBar.innerHTML = createNavbar()
